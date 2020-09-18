f5045ca87827fc5a205be08453ac3e4130031659
daejo <daejo@icloud.com>
2020-09-17 20:35:02 -0700

    adds writefile function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const { generateMarkdown } = require('./utils/generate-Markdown.js');
 const inquirer = require("inquirer"); //declaring inquirer is needed for app to work.
+const fs = require("fs");
 
 /* QUESTIONS ARRAY */
 const questions = () => {
@@ -139,9 +140,26 @@ const questions = () => {
 
 questions();
 
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
+// function to write README file
+function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+          // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+          if (err) {
+            reject(err);
+            // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+            return;
+          }
+    
+          // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+          resolve({
+            ok: true,
+            message: 'File created!'
+          });
+        });
+    });
+
+}
 
 // // function to initialize program
 // function init() {
